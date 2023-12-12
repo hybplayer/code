c3889df0b2a1de467a3dc136eb6d6dddcdc3612b
hybplayer <1161050535@qq.com>
2023-12-13 02:17:16 +0800

    relearn

---
+++ build/CMakeFiles/test.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.cpp.dir/test.obj"
+  "CMakeFiles/test.cpp.dir/test.obj.d"
+  "libtest.cpp.dll.a"
+  "test.cpp.exe"
+  "test.cpp.exe.manifest"
+  "test.cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cout << "over" << endl;
+    return 0;
+}
