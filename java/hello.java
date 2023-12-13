3a904d21f21a7be79324bb14ecfaa29626a45973
hybplayer <1161050535@qq.com>
2023-12-13 22:07:34 +0800

    java

---
+++ java/hello.java
@@ -0,0 +1,19 @@
+public class hello {
+    /**
+     * hello world
+     * @param args null
+     */
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        int a = 0;
+        final int b = 1;
+        String myname = "hyb";
+        String sad = " is sad";
+        String WhatIWan = myname + sad;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(myname);
+        System.out.println(WhatIWan);
+        System.out.println(1 + myname);
+    }
+}
