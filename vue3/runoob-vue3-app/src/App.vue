8d9e6cf6d8fa9d4aeadbc45b24da462702a1b4f4
hybplayer <1161050535@qq.com>
2024-02-23 01:40:16 +0800

    nodejs and vue3

---
+++ vue3/runoob-vue3-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="欢迎来到菜鸟教程！" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
