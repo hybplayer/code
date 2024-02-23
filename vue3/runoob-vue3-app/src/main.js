91d0a2d5d6ed7c31143dd95b0b22e5fecd7636de
hybplayer <1161050535@qq.com>
2024-02-23 20:56:34 +0800

    read vue3

--- vue3/runoob-vue3-app/src/main.js
+++ vue3/runoob-vue3-app/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import HelloRunoob from './components/HelloRunoob.vue'
+
+const app = createApp(App)
+app.component('hello-runoob', HelloRunoob) // 自定义标签
+app.mount('#app')
