6244362e926216b3190ba573c94b618c355fcf24
hybplayer <1161050535@qq.com>
2024-02-25 00:46:25 +0800

    liaoxuefeng java

---
+++ java/com/hyb/hello/hello.java
@@ -0,0 +1,32 @@
+package com.hyb.hello;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author : hyb
+ * @date ：2024-02-23 - 21:46
+ * @desc :
+ */
+public class hello {
+    public static void main(String[] args) {
+        printClassInfo("".getClass());
+        printClassInfo(Runnable.class);
+        printClassInfo(java.time.Month.class);
+        printClassInfo(String[].class);
+        printClassInfo(int.class);
+    }
+
+    static void printClassInfo(Class cls) {
+        System.out.println("Class name: " + cls.getName());
+        System.out.println("Simple name: " + cls.getSimpleName());
+        if (cls.getPackage() != null) {
+            System.out.println("Package name: " + cls.getPackage().getName());
+        }
+        System.out.println("is interface: " + cls.isInterface());
+        System.out.println("is enum: " + cls.isEnum());
+        System.out.println("is array: " + cls.isArray());
+        System.out.println("is primitive: " + cls.isPrimitive());
+        System.out.println("--------------------------------------------");
+    }
+}
